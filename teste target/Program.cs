6b2aeac3d99aba638ccd27b969a0cce9cf15a30b
Otavio <otaviolucasolv@gmail.com>
2024-09-24 22:43:51 -0300

    Adicionar arquivos de projeto.

---
+++ teste target/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace teste_target
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "eu quero entrar na tArget";
+            int contA = 0;
+
+            for (int i = 0; i < texto.Length; i++) //o for vai percorrer o texto
+            {
+                if (texto[i] == 'a' || texto[i] == 'A')//se "a" ou "A" for encontrado, vai ser incrementado no contA
+           
+                    contA++;
+               
+            }
+
+            Console.WriteLine($"A letra 'A' apareceu um total de {contA} vezes na string");
+        }
+    }
+}
